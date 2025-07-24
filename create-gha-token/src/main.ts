1f716bce0bb8e79b8967f37cffa64fe1d404386c
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-07-24 14:01:20 +0900

    feat: create-gha-token

---
+++ create-gha-token/src/main.ts
@@ -0,0 +1,68 @@
+import {
+  getInput,
+  setFailed,
+  setOutput,
+  saveState,
+  setSecret,
+  info,
+  debug,
+} from "@actions/core"
+
+import * as jwt from "jsonwebtoken"
+
+const main = async () => {
+  info("==> Getting the inputs...")
+  const GITHUB_CLIENT_ID = getInput("app-id") || getInput("app_id")
+  const GITHUB_PRIVATE_KEY = getInput("private-key") || getInput("private_key")
+
+  const repositories = 
+    getInput("repositories") ||
+    String(process.env.GITHUB_REPOSITORY).split("/")[1]
+
+  const payload = {
+      iat: Math.floor(Date.now() / 1000) - 1 * 60, // Issues 60 seconds in the past
+      exp: Math.floor(Date.now() / 1000) + 1 * 60, // Expire 10 min in the feature
+      iss: GITHUB_CLIENT_ID,
+  };
+
+  const access_repositories = JSON.Stringify({
+    repositories: [`${repositories}`],
+  });
+
+  const resultJWT = jwt.sign(payload, GITHUB_PRIVATE_KEY, {
+    algorithm: "RS256",
+  })
+
+  const url = "https://api.github.com/app/installations";
+  const resultInstallation = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${resultJWT}`,
+      Accept: "application/vnd.github.v3+json",
+              "X-GitHub-Api-Version": "2022-11-28"
+    }
+  });
+
+  const installations = await resultInstallation.json();
+  const installation = installations[0]
+
+  const bot_commit_sign = {
+    name: installation.app_slug,
+    email: `${installation.app_id}+${installation.app_slug}@users.noreply.github.com`
+  };
+
+  const resultAccessToken = await fetch(installation.access_tokens_url, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${resultJWT}`,
+      Accept: "application/vnd.github.v3+json",
+              "X-GitHub-Api-Version": "2022-11-28"
+    },
+    body: access_repositories
+  });
+
+  const accessToken = await resultAccessToken.json();
+
+  if (!accessToken) {}
+
+}
