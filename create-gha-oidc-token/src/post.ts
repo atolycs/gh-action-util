9bdb14ffc7ee9d7346c62bd61b03339ab266621b
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-08-28 18:56:23 +0900

    feat: add post

---
+++ create-gha-oidc-token/src/post.ts
@@ -0,0 +1,38 @@
+import {
+  debug,
+  info,
+  getState,
+  setFailed,
+} from "@actions/core"
+import { HttpClient } from "@actions/http-client"
+
+async function postRun() {
+  try {
+    const apiUrl = process.env["GITHUB_API_URL"] || "https://api.github.com"
+    const client = new HttpClient("actions-github-app-token")
+
+    const token = getState("token")
+
+    if (!token) {
+      info("==> Token is revoked. Skip Post Action");
+      return 0;
+    }
+
+    const resp = await client.del(`${apiUrl}/installation/token`, {
+      Authorization: `token ${token}`,
+      Accept: "application/vnd.github+json"
+    })
+
+    const statusCode = resp.message.statusCode
+
+    if (statusCode === 204) {
+      info("===> Token revoked succuessfully.")
+      return 0;
+    }
+    const body = await resp.readBody()
+
+    info(`==> [WARN] unexpected ${statusCode}, ${body}`)
+  } catch (error) {
+    setFailed(`${error}`)
+  }
+}
